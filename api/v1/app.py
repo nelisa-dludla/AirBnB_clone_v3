83a40c6f6ef02c314f8e3afc99cd1ce153a87f2b
Nelisa <nelisa.dludla@outlook.com>
2024-02-27 01:26:04 +0200

    Updated /api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
 
-
 from flask import Flask, jsonify, make_response, render_template
 from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask_cors import CORS
-'''
-from flasgger import Swagger
-from flasgger.utils import swag_from
-'''
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*":{"origins":"0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
 def not_found(err):
     return make_response(jsonify({"error": "Not found"}), 404)
-
-'''
-app.config["SWAGGER"] = {
-        'title': 'AirBnB clone REstful API',
-        'uiversion': 3
-        }
-Swagger(app)
-'''
 
 
 @app.teardown_appcontext
